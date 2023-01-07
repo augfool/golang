861a30a125045173bd83f7bdac8407323604cef2
augfool <11628086@qq.com>
2023-01-07 23:22:04 +0800

    1

---
+++ container/demo/wordpress.sh
@@ -0,0 +1,28 @@
+
+docker pull wordpress:5
+docker pull mariadb:10
+docker pull nginx:alpine
+
+
+docker run -d --rm \
+    --env MARIADB_DATABASE=db \
+    --env MARIADB_USER=wp \
+    --env MARIADB_PASSWORD=123 \
+    --env MARIADB_ROOT_PASSWORD=123 \
+    mariadb:10
+
+docker exec -it 9ac mysql -u wp -p
+
+
+docker run -d --rm \
+    --env WORDPRESS_DB_HOST=172.17.0.2 \
+    --env WORDPRESS_DB_USER=wp \
+    --env WORDPRESS_DB_PASSWORD=123 \
+    --env WORDPRESS_DB_NAME=db \
+    wordpress:5
+
+
+docker run -d --rm \
+    -p 80:80 \
+    -v `pwd`/wp.conf:/etc/nginx/conf.d/default.conf \
+    nginx:alpine
